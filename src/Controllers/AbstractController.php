353e8ed6aa42c716c181974c6f9d27744c8737e6
shucream0117 <shucream0117@users.noreply.github.com>
2020-05-13 04:06:54 +0900

    add RequestUtil class

--- src/Controllers/AbstractController.php
+++ src/Controllers/AbstractController.php
@@ -5,16 +5,13 @@ declare(strict_types=1);
 namespace Shucream0117\PhalconLib\Controllers;
 
 use Phalcon\Mvc\Controller;
+use Shucream0117\PhalconLib\Utils\Http\RequestUtil;
 
 abstract class AbstractController extends Controller
 {
     protected function getRemoteAddress(): string
     {
-        $request = $this->request;
-        if ($ip = $request->getServer('HTTP_X_FORWARDED_FOR')) {
-            return $ip;
-        }
-        return $request->getServer('REMOTE_ADDR');
+        return RequestUtil::getRemoteAddress($this->request);
     }
 
     public function onConstruct()
