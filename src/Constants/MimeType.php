cae8e52ef442b49760915427242aaee438fc5893
shucream0117 <shucream0117@users.noreply.github.com>
2020-06-17 06:09:18 +0900

    add helper method to MimeType class

--- src/Constants/MimeType.php
+++ src/Constants/MimeType.php
@@ -18,4 +18,28 @@ class MimeType
     const WEBM = 'video/webm';
 
     const JSON = 'application/json';
+
+    /**
+     * 大文字小文字の決まりがないため、比較メソッドを用意
+     * @param string $a
+     * @param string $b
+     * @return bool
+     */
+    public static function isSame(string $a, string $b): bool
+    {
+        return strtolower($a) === strtolower($b);
+    }
+
+    /**
+     * $mimeType が $mimeTypes に含まれるかどうか。
+     * (大文字小文字の差異を考慮)
+     * @param string $mimeType
+     * @param array $mimeTypes
+     * @return bool
+     */
+    public static function isIncluded(string $mimeType, array $mimeTypes): bool
+    {
+        $mimeTypes = array_map(fn(string $m) => strtolower($m), $mimeTypes);
+        return in_array(strtolower($mimeType), $mimeTypes);
+    }
 }
