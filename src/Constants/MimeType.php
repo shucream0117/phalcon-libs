40887dae494a26da0130c1c3654e8d5c85ebfce1
shucream0117 <shucream0117@users.noreply.github.com>
2020-06-17 05:27:03 +0900

    add MimeType constants class

---
+++ src/Constants/MimeType.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+class MimeType
+{
+    const JPEG = 'image/jpeg';
+    const PNG = 'image/png';
+    const GIF = 'image/gif';
+    const WEBP = 'image/webp';
+    const SVG = 'image/svg+xml';
+    const BMP = 'image/bmp';
+
+    const MP4 = 'video/mp4';
+    const MOV = 'video/quicktime';
+    const WEBM = 'video/webm';
+}
