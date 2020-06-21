b5ebc2f98d4ea36ba309b4dfcb1fb47333b8c0c7
shucream0117 <shucream0117@users.noreply.github.com>
2020-06-22 03:37:20 +0900

    fix

--- src/Services/QueueManager/RedisQueueManager.php
+++ src/Services/QueueManager/RedisQueueManager.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Shucream0117\PhalconLib\Services\QueueManager;
 
 use Enqueue\Redis\RedisConnectionFactory;
+use Shucream0117\PhalconLib\Entities\RedisInfo;
 
 /**
  * php-enqueue でRedisを使ったキュー処理をする時に使う氏
@@ -12,20 +13,18 @@ use Enqueue\Redis\RedisConnectionFactory;
  */
 class RedisQueueManager extends AbstractQueueManager
 {
-    private string $host;
-    private int $port;
+    private RedisInfo $redisInfo;
 
-    public function __construct(string $host, int $port)
+    public function __construct(RedisInfo $redisInfo)
     {
-        $this->host = $host;
-        $this->port = $port;
+        $this->redisInfo = $redisInfo;
     }
 
     protected function getFactory(): RedisConnectionFactory
     {
         return new RedisConnectionFactory([
-            'host' => $this->host,
-            'port' => $this->port,
+            'host' => $this->redisInfo->getHost(),
+            'port' => $this->redisInfo->getPort(),
             'scheme_extensions' => ['phpredis'],
         ]);
     }
