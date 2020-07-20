d8fd7f94ed8d55e9e1b8f8986c56634ca6b35382
shucream0117 <shucream0117@users.noreply.github.com>
2020-07-21 05:40:53 +0900

    update AbstractModel

--- src/Models/AbstractModel.php
+++ src/Models/AbstractModel.php
@@ -9,7 +9,7 @@ use Phalcon\Mvc\Model;
 abstract class AbstractModel extends Model
 {
     public static string $COLUMN_ID = 'id';
-    
+
     abstract public static function getTableName(): string;
 
     /**
@@ -25,5 +25,17 @@ abstract class AbstractModel extends Model
      */
     public function onConstruct(): void
     {
+    }
+
+    /**
+     * getTableName()ではなく、クラス名で解決される
+     * @param string $columnName
+     * @param string|null $alias
+     * @return string
+     */
+    public static function getFullyQualifiedColumnName(string $columnName, ?string $alias = null): string
+    {
+        $tableName = $alias ?: static::class;
+        return "{$tableName}.{$columnName}";
     }
 }
