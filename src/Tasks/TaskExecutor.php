d07e10ba08b882eb01f19a8cba2a7103a1398592
shucream0117 <shucream0117@users.noreply.github.com>
2020-08-13 01:08:48 +0900

    edit TaskExecutor

--- src/Tasks/TaskExecutor.php
+++ src/Tasks/TaskExecutor.php
@@ -5,7 +5,6 @@ declare(strict_types=1);
 namespace Shucream0117\PhalconLib\Tasks;
 
 use Closure;
-use InvalidArgumentException;
 use Phalcon\Cli\Console;
 use Throwable;
 
@@ -15,7 +14,7 @@ use Throwable;
  * TaskExecutor::execute の引数に渡す。
  *
  * 例えばTasks/UserTaskを起動する場合、
- * php app/Tasks/executor.php UserTask arg1 arg2... というコマンドを発行する。
+ * php app/Tasks/executor.php --task=UserTask --hoge=fuga というコマンドを発行する。
  *
  * Tasks/QueueProcessors/UserTask のようにTasks下で更にディレクトリ配下にあるクラスを実行する場合は、
  * php app/Tasks/executor.php QueueProcessors\\UserTask とする。
@@ -28,30 +27,24 @@ use Throwable;
  * actionをmainに固定するなど、多少シンプルになるように改変しているので注意。
  *
  */
+
 class TaskExecutor
 {
     /**
      * @param Console $app
-     * @param array $argv
      * @param Closure|null $onError function(Throwable $t):void な関数
      */
-    public static function execute(Console $app, array $argv, ?\Closure $onError = null): void
+    public static function execute(Console $app, ?\Closure $onError = null): void
     {
-        if (count($argv) < 2) {
-            throw new InvalidArgumentException('task name should be specified');
+        $options = getopt('', ['task:']);
+        if (!$taskName = ($options['task'] ?? null)) {
+            throw new \Exception('option --task is required');
         }
-        // actionはmainで固定する。
-        $arguments = ['action' => 'main'];
-        foreach ($argv as $k => $arg) {
-            if ($k === 1) {
-                $arguments['task'] = preg_replace('/Task$/i', '', $arg);
-            } elseif ($k >= 2) {
-                $arguments['params'][] = $arg;
-            }
-        }
-
         try {
-            $app->handle($arguments);
+            $app->handle([
+                'task' => preg_replace('/Task$/i', '', $taskName),
+                'action' => 'main', // actionはmainで固定する。
+            ]);
         } catch (Throwable $throwable) {
             if ($onError) {
                 $onError($throwable);
