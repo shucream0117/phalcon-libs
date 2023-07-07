7e20d0c3ba1ac3f81e6b64c36f56f58b025d150a
shucream0117 <shucream0117@users.noreply.github.com>
2023-07-08 04:21:42 +0900

    ImageUtil機能追加

--- src/Utils/ImageUtil.php
+++ src/Utils/ImageUtil.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 namespace Shucream0117\PhalconLib\Utils;
 
-use Phalcon\Image\Adapter\AbstractAdapter;
+use Phalcon\Image\Adapter\AdapterInterface;
 use Phalcon\Image\Enum;
 use Shucream0117\PhalconLib\Constants\MimeType;
 
@@ -12,11 +12,11 @@ class ImageUtil
 {
     /**
      * アスペクト比を保ったまま、長辺を$maxSizePxとしてリサイズする
-     * @param AbstractAdapter $image
+     * @param AdapterInterface $image
      * @param int $maxSizePx
-     * @return AbstractAdapter
+     * @return AdapterInterface
      */
-    public static function resizeWithSameAspectRatio(AbstractAdapter $image, int $maxSizePx): AbstractAdapter
+    public static function resizeWithSameAspectRatio(AdapterInterface $image, int $maxSizePx): AdapterInterface
     {
         $width = $image->getWidth();
         $height = $image->getHeight();
@@ -27,16 +27,42 @@ class ImageUtil
     }
 
     /**
-     * @param AbstractAdapter $image
+     * @param AdapterInterface $image
      * @param string $colorCode
-     * @return AbstractAdapter
+     * @return AdapterInterface
      */
-    public static function fillColor(AbstractAdapter $image, string $colorCode = '#ffffff'): AbstractAdapter
+    public static function fillColor(AdapterInterface $image, string $colorCode = '#ffffff'): AdapterInterface
     {
         // pngの場合の透過部分白埋め
         if (MimeType::isEqual($image->getMime(), MimeType::PNG)) {
             $image->background($colorCode);
         }
+        return $image;
+    }
+
+    /**
+     * 短辺の長さで中央から正方形に切り抜く
+     * @param AdapterInterface $image
+     * @return AdapterInterface
+     */
+    public static function cropSquare(AdapterInterface $image): AdapterInterface
+    {
+        $imageWidth = $image->getWidth();
+        $imageHeight = $image->getHeight();
+
+        if ($imageWidth === $imageHeight) {
+            return $image;
+        }
+        if ($imageWidth < $imageHeight) { // 縦長
+            $length = $imageWidth;
+            $offsetX = 0;
+            $offsetY = ($imageHeight - $imageWidth) / 2;
+        } else { // 横長
+            $length = $imageHeight;
+            $offsetX = ($imageWidth - $imageHeight) / 2;
+            $offsetY = 0;
+        }
+        $image->crop($length, $length, (int)round($offsetX), (int)round($offsetY));
         return $image;
     }
 }
